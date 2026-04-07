d5f544f37efd9629e545ff049e57ee7819a1ff3a
Thanu <88thanu@gmail.com>
2026-04-08 00:59:05 +0530

    _

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {"status": "Phase 1 complete", "vm": "running in private VNet - Central_India_New"}
+    return {"status": "Phase 1 complete", "vm": "running in private VNet - Central_India"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
